137d42613041ad88ac08d53823ad9ff7b2b4a43c
rijan <hrijon.chp@gmail.com>
2020-03-09 14:31:08 +0800

    Fixed toggle button

--- js/script.js
+++ js/script.js
@@ -10,17 +10,17 @@ $(document).ready(function () {
     });
 });
 
-$("button").click(function(e){
-    e.preventDefault(); // this will prevent the anchor tag from going the user off to the link
-    var bookmarkUrl = "your_bookmark_url";
-    var bookmarkTitle = "your_bookmark_title";
+// $("button").click(function(e){
+//     e.preventDefault(); // this will prevent the anchor tag from going the user off to the link
+//     var bookmarkUrl = "your_bookmark_url";
+//     var bookmarkTitle = "your_bookmark_title";
 
-    if (window.sidebar) { // For Mozilla Firefox Bookmark
-        window.sidebar.addPanel(bookmarkTitle, bookmarkUrl,"");
-    } else if( window.external || document.all) { // For IE Favorite
-        window.external.AddFavorite( bookmarkUrl, bookmarkTitle);
-    } else { // for other browsers which does not support
-         alert('Your browser does not support this bookmark action');
-         return false;
-    }
-  });
+//     if (window.sidebar) { // For Mozilla Firefox Bookmark
+//         window.sidebar.addPanel(bookmarkTitle, bookmarkUrl,"");
+//     } else if( window.external || document.all) { // For IE Favorite
+//         window.external.AddFavorite( bookmarkUrl, bookmarkTitle);
+//     } else { // for other browsers which does not support
+//          alert('Your browser does not support this bookmark action');
+//          return false;
+//     }
+//   });
